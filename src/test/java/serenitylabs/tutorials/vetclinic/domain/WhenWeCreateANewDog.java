d2da9672d48738eb56b5e13073b45cc5d7f0a81a
John Smart <john.smart@wakaleo.com>
2016-08-01 07:11:16 +0530

    Implementation of a simple builder

---
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -0,0 +1,15 @@
+package serenitylabs.tutorials.vetclinic.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WhenWeCreateANewDog {
+    @Test
+    public void a_new_dog_should_have_a_name_and_a_breed_and_a_colour() throws Exception {
+        Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
+
+        Assert.assertEquals("Fido",fido.getName());
+        Assert.assertEquals("Labrador", fido.getBreed());
+        Assert.assertEquals("Black", fido.getColour());
+    }
+}
