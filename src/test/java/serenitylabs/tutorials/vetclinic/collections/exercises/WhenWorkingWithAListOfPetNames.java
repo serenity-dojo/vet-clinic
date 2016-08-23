9531c1f0ade9226d555f9b7f0c175eba1fe8dd17
Kiran1552 <kiran.mahale@capgemini.com>
2016-08-23 20:15:31 +0530

    1st exercise  "WhenWorkingWithAListOfPetNames" of kata/collections is completed

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAListOfPetNames.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAListOfPetNames.java
@@ -3,8 +3,7 @@ package serenitylabs.tutorials.vetclinic.collections.exercises;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertThat;
@@ -16,6 +15,8 @@ public class WhenWorkingWithAListOfPetNames {
         List<String> names = Lists.newArrayList();
 
         // TODO
+        // Add element in List using add() method
+        names.add("Fido");
 
         assertThat(names, hasItems("Fido"));
     }
@@ -23,7 +24,10 @@ public class WhenWorkingWithAListOfPetNames {
     @Test
     public void should_remove_Fido_from_the_list_of_pets() {
         List<String> names = Lists.newArrayList("Felix","Fido","Spot");
+
         // TODO
+        // Removing the element from List using by element
+        names.remove("Fido");
 
         assertThat(names, hasItems("Felix","Spot"));
     }
@@ -33,6 +37,8 @@ public class WhenWorkingWithAListOfPetNames {
         List<String> names = Lists.newArrayList("Felix","Fido","Spot");
 
         // TODO
+        // Removing 1st element from List using index
+        names.remove(0);
 
         assertThat(names, hasItems("Fido","Spot"));
     }
@@ -42,8 +48,12 @@ public class WhenWorkingWithAListOfPetNames {
         List<String> cats = Lists.newArrayList("Felix","Spot");
         List<String> dogs = Lists.newArrayList("Fido","Rover");
 
+
         // TODO
-        List<String> catsAndDogs = null;
+        // Creating list of catsAndDogs, assigning cats List to catsAndDogs
+        // Add dogs list in catsAndDogs List
+        List<String> catsAndDogs = new ArrayList(cats);
+        catsAndDogs.addAll(dogs);
 
         assertThat(catsAndDogs, hasItems("Felix","Spot","Fido","Rover"));
     }
@@ -54,7 +64,11 @@ public class WhenWorkingWithAListOfPetNames {
         List<String> dogs = Lists.newArrayList("Fido","Rover");
 
         // TODO
-        List<String> catsAndDogs = null;
+        List<String> catsAndDogs = new ArrayList<>();
+        // System.out.print( cats.stream().findFirst());
+        catsAndDogs.add(cats.get(0));
+        catsAndDogs.addAll(dogs);
+        catsAndDogs.add(cats.get(1));
 
         assertThat(catsAndDogs, hasItems("Felix","Fido","Rover","Spot"));
     }
@@ -64,6 +78,8 @@ public class WhenWorkingWithAListOfPetNames {
         List<String> pets = Lists.newArrayList("Felix","Spot","Fido","Rover");
 
         // TODO
+        // Sort method is used for sorting the list, defaultly used assending order
+        Collections.sort(pets);
 
         assertThat(pets, hasItems("Felix","Fido","Rover","Spot"));
     }
@@ -73,6 +89,8 @@ public class WhenWorkingWithAListOfPetNames {
         List<String> pets = Lists.newArrayList("Felix","Spot","Fido","Rover");
 
         // TODO
+        Collections.sort(pets, Collections.reverseOrder(String.CASE_INSENSITIVE_ORDER));
+        // System.out.print(pets.toString());
 
         assertThat(pets, hasItems("Spot","Rover","Fido","Felix"));
     }
