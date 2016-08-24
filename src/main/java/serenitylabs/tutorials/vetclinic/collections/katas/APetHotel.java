f90b9ce45b5c503b057d8c80c07f1ef7c2b29c3a
Saket Puranik <saket@Sakets-MacBook-Air.local>
2016-08-25 01:50:32 +0530

    Added more scenarios in kata

--- src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/APetHotel.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/APetHotel.java
@@ -1,28 +1,24 @@
 package serenitylabs.tutorials.vetclinic.collections.katas;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableList;
-
-import serenitylabs.tutorials.vetclinic.Breed;
 import serenitylabs.tutorials.vetclinic.Pet;
 
 /**
  * A utility class to generate pet hotels with pets already booked
  */
 public class APetHotel {
-	
 
-	private String name;
-	private List<Pet> pets = new ArrayList<>() ;
+	private static String name;
+	private Set<Pet> pets = new HashSet<>();
 	
 	public String getName() {
 		return name;
 	}
 
-	public List<Pet> getPets() {
+	public Set<Pet> getPets() {
 		return pets;
 	}
 	
@@ -30,51 +26,30 @@ public class APetHotel {
          this.name = name;
     }
 
-	public APetHotel(List<Pet> pets2) {
-		pets.addAll(pets2);
+	
+
+	public APetHotel( Set<Pet> pets2 ) {
+		pets.addAll( pets2 );
 	}
 
-	public static PetAdder with(int petCount) {
-        return new PetAdder(petCount);
-    }
+	public APetHotel checkIN(Set<Pet> pets2) {
+		return new APetHotel(pets2);
+	}
 
-	
+
+	public void sort() {
+		pets = pets.stream().sorted((Pet o1, Pet o2)->o1.getName().compareTo(o2.getName())).collect(Collectors.toSet());
+	}
+
     public static AHotelBuilder aHotel( ) {
         return new AHotelBuilder();
     }
-    public static class PetAdder {
-        private final int petCount;
-
-        public PetAdder(int petCount) {
-
-            this.petCount = petCount;
-        }
-        
-        private Pet somePet(int petCount) {
-            return new Pet(someName(petCount), someBreed());
-        }
-
-        private final static Random random = new Random();
-
-        private Breed someBreed() {
-            return Breed.values()[ random.nextInt(Breed.values().length) ];
-        }
-
-        private final static List<String> PET_NAMES = ImmutableList.of("Fido","Felix","Rover","Spot");
-
-        private String someName(int petCount) {
-            return PET_NAMES.get(random.nextInt(PET_NAMES.size())) + " " + petCount;
-        }
-    }
     
     public static class AHotelBuilder {
-    	public APetHotel named( String name ){
+
+		public APetHotel named( String name ){
     		return new APetHotel( name );
     	}
-    	
-    	public static APetHotel checkIn(List<Pet> pets) {
-            return new APetHotel(pets);
-        }
     	
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/collections/katas/WhenBookingPetsIntoAPetHotel.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/katas/WhenBookingPetsIntoAPetHotel.java
@@ -1,43 +1,48 @@
 package serenitylabs.tutorials.vetclinic.collections.katas;
 
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.List;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import serenitylabs.tutorials.vetclinic.Pet;
-
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+
+import serenitylabs.tutorials.vetclinic.Pet;
 
 public class WhenBookingPetsIntoAPetHotel {
 
 
+	 APetHotel hotel;
+	 Set<Pet> pets ;
+	 
+	@Before
+	public void init(){
+       hotel = APetHotel.aHotel().named("Silver Inn");
+   	   pets = hotel.getPets();
+		
+	}
     @Test
     public void the_hotel_should_initially_have_no_pets_booked() {
     	
-    	  APetHotel hotel = APetHotel.aHotel().named("Silver Inn");
-    	  List<Pet> pets = hotel.getPets();
           assertThat(pets, emptyCollectionOf(Pet.class));
     }
 
     @Test
     public void should_be_able_to_check_a_pet_into_the_hotel() throws Exception {
-    	List<Pet> pets = Lists.newArrayList();
     	Pet pet = Pet.cat().named("Felix");
     	pets.add(pet);
-    	assertThat(pets.size(), equalTo(1));
+    	hotel.checkIN( pets );
+    	assertThat(hotel.getPets().size(), equalTo(1));
     }
 
     @Test
     public void should_be_able_to_check_in_several_pets() throws Exception {
-    	List<Pet> pets = Lists.newArrayList();
     	Pet cat1 = Pet.cat().named("Felix");
     	Pet cat2=Pet.cat().named("Helix");
     	Pet dog1 = Pet.dog().named("Bruno");
@@ -46,7 +51,8 @@ public class WhenBookingPetsIntoAPetHotel {
     	pets.add(cat2);
     	pets.add(dog1);
     	pets.add(dog2);
-    	assertThat(pets.size(),greaterThan(1));
+    	hotel.checkIN( pets );
+    	assertThat(hotel.getPets().size(),greaterThan(1));
     }
 
     @Test
@@ -60,20 +66,20 @@ public class WhenBookingPetsIntoAPetHotel {
     	pets.add(cat2);
     	pets.add(dog1);
     	pets.add(dog2);
-    	assertThat(pets.size(), equalTo(3));
+    	hotel.checkIN( pets );
+    	assertThat(hotel.getPets().size(), equalTo(3));
     }
 
     @Test
     public void should_be_able_to_retrieve_checked_in_pets_in_alphabetical_order() throws Exception {
-    	List<Pet> pets = Lists.newArrayList();
     	Pet cat1 = Pet.cat().named("Felix");
     	Pet cat2=Pet.cat().named("Helix");
     	Pet dog1 = Pet.dog().named("Bruno");
     	pets.add(cat1);
     	pets.add(cat2);
     	pets.add(dog1);
-    	pets.sort((Pet o1, Pet o2)->o1.getName().compareTo(o2.getName()));
-    	assertThat(pets, contains("Felix","Helix","Bruno"));
+    	hotel.checkIN(pets).sort();
+    	assertThat( hotel.getPets().stream().findFirst().get() ,is(equalTo(dog1)));
     }
 
     @Test
