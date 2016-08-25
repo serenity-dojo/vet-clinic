8198565884a72e3e8a3551b158557b3f16cbb30b
sayali-kotkar <sayalikotkar@gmail.com>
2016-08-25 12:08:23 +0530

    Update Dog.java

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -43,23 +43,12 @@ public class Dog {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getBreed() {
         return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
     }
 
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
