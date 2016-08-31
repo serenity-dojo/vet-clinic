ac889e46daeb61ba49cadd9b9246944b1e2b8838
John Smart <john.smart@wakaleo.com>
2016-08-31 21:28:08 +0100

    Prepared tutorial starting point

--- src/test/java/serenitylabs/tutorials/vetclinic/screenplay/NotAHotelManagerException.java
+++
@@ -1,7 +0,0 @@
-package serenitylabs.tutorials.vetclinic.screenplay;
-
-public class NotAHotelManagerException extends RuntimeException {
-    public NotAHotelManagerException(String name) {
-        super(name + " is not a hotel manager");
-    }
-}
