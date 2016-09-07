2fff247d6a7d9e6c6bef697051733bcb48987c0b
John Smart <john.smart@wakaleo.com>
2016-09-07 17:09:48 +0100

    Initial starting point

--- src/test/java/serenitylabs/tutorials/vetclinic/webdriver/WhenInteractingWithElementsAndForms.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/webdriver/WhenInteractingWithElementsAndForms.java
@@ -1,13 +1,10 @@
 package serenitylabs.tutorials.vetclinic.webdriver;
 
-import net.serenitybdd.core.annotations.findby.By;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,10 +27,7 @@ public class WhenInteractingWithElementsAndForms {
 
         // TODO: Enter 'Town Hall' into the 'From' field
 
-        WebElement inputField = driver.findElement(By.id("display_origin"));
-        inputField.sendKeys("Town Hall");
-
-        String displayedValue = inputField.getAttribute("value");
+        String displayedValue = null;
         assertThat(displayedValue, equalTo("Town Hall"));
     }
 
@@ -42,41 +36,24 @@ public class WhenInteractingWithElementsAndForms {
     public void click_on_a_button() {
         // TODO: Click on the View Trip button
 
-        driver.findElement(By.id("display_origin")).sendKeys("Town Hall");
-        driver.findElement(By.id("display_destination")).sendKeys("Parramatta");
-
-        WebElement viewTrip =  driver.findElement(By.id("btnTripPlannerSubmit"));
-
-        assertThat(viewTrip.isEnabled(), is(true));
-
-        viewTrip.click();
-
-        String title = driver.findElement(By.tagName("h1")).getText();
+        String title = null;
         assertThat(title, is("Plan your trip"));
     }
 
     @Test
     public void click_on_a_checkbox() {
         // TODO: Click on the Remember Me checkbox
-        WebElement rememberMe = driver.findElement(By.id("chkRemember"));
 
-        rememberMe.click();
-
-        boolean rememberMeChoice = rememberMe.isSelected();
+        Boolean rememberMeChoice = null;
         assertThat(rememberMeChoice, is(true));
     }
 
     @Test
     public void click_on_a_radio_button() {
 
-        WebElement leaveAfter = driver.findElement(By.cssSelector("[value='arr']"));
-
         // TODO: Choose the 'Leave After' option and make sure it is the one selected
-        leaveAfter.click();
 
-        assertThat(leaveAfter.isSelected(), is(true));
-
-        String leaveOrArrivePreference = leaveAfter.getAttribute("value");
+        String leaveOrArrivePreference = null;
 
         assertThat(leaveOrArrivePreference, is("arr"));
     }
@@ -86,11 +63,7 @@ public class WhenInteractingWithElementsAndForms {
     public void choose_a_dropdown_value() {
         // TODO: Click on 12pm
 
-        Select dateList = new Select(driver.findElement(By.id("itdTimeHour")));
-
-        dateList.selectByVisibleText("12");
-
-        String selectedHour = dateList.getFirstSelectedOption().getText();
+        String selectedHour = null;
 
         assertThat(selectedHour, is("12"));
     }
