0dc003b4e7b8aaf3a8a8e72bf9299a9475a6d865
sayali-kotkar <sayalikotkar@gmail.com>
2016-09-08 23:17:22 +0530

    Implementing map to remove if-else

--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/SportsSchedule.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/SportsSchedule.java
@@ -1,26 +1,29 @@
 package serenitylabs.tutorials.vetclinic.playingball.model;
 
+import com.google.common.collect.Maps;
+
 import java.applet.Applet;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Map;
 
 /**
  * Created by siddharthk on 9/7/2016.
  */
 public class SportsSchedule {
 
+
     RecreationalSportsSchedule recretionalSportsSchedule = new RecreationalSportsSchedule();
-    SchoolSportsSchedule schoolSportsSchedule = new SchoolSportsSchedule();
+    private static final Map<DayOfWeek,GameSchedule> scheduleMap = Maps.newHashMap();
+    static {
+        scheduleMap.put(DayOfWeek.WEDNESDAY, new SchoolSportsSchedule());
+    }
 
     public PlayBall forDate(LocalDate someDay) {
 
-        if (someDay.getDayOfWeek() == DayOfWeek.WEDNESDAY) {
-            return schoolSportsSchedule.forGameOn(someDay);
+        GameSchedule gameSchedule = scheduleMap.getOrDefault(someDay.getDayOfWeek(), recretionalSportsSchedule);
+        return  gameSchedule.forGameOn(someDay);
 
-        } else {
-            return recretionalSportsSchedule.forGameOn(someDay);
-
-        }
     }
 
 }
