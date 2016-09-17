9c99ac36e471f75e99e4ba5d0c24ff28905f2db6
sayali-kotkar <sayalikotkar@gmail.com>
2016-09-17 23:31:09 +0530

    webdriver-elements

--- src/test/java/serenitylabs/tutorials/vetclinic/webdriver/WhenInteractingWithElementsAndForms.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/webdriver/WhenInteractingWithElementsAndForms.java
@@ -2,12 +2,18 @@ package serenitylabs.tutorials.vetclinic.webdriver;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -15,9 +21,16 @@ import static org.hamcrest.core.Is.is;
 public class WhenInteractingWithElementsAndForms {
     WebDriver driver;
 
+    @BeforeClass()
+    public static void setUpClass(){
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\siddharthk\\Downloads\\chromedriver_win32\\chromedriver.exe");
+    }
+
     @Before
     public void setup() {
-        driver = new PhantomJSDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("http://www.sydneytrains.info/");
     }
@@ -25,9 +38,10 @@ public class WhenInteractingWithElementsAndForms {
     @Test
     public void entering_a_field_value() {
 
-        // TODO: Enter 'Town Hall' into the 'From' field
+        WebElement fromElement = driver.findElement(By.id("display_origin"));
+        fromElement.sendKeys("Town Hall");
 
-        String displayedValue = null;
+        String displayedValue = fromElement.getAttribute("value");
         assertThat(displayedValue, equalTo("Town Hall"));
     }
 
@@ -35,17 +49,22 @@ public class WhenInteractingWithElementsAndForms {
     @Test
     public void click_on_a_button() {
         // TODO: Click on the View Trip button
+        driver.findElement(By.id("display_origin")).sendKeys("Town Hall");
+        driver.findElement(By.id("display_destination")).sendKeys("Parramatta");
 
-        String title = null;
-        assertThat(title, is("Plan your trip"));
+        WebElement btnTripPlannerSubmit = driver.findElement(By.id("btnTripPlannerSubmit"));
+        btnTripPlannerSubmit.click();
+
+        assertThat(driver.getTitle(), containsString("Trip results"));
     }
 
     @Test
     public void click_on_a_checkbox() {
         // TODO: Click on the Remember Me checkbox
 
-        Boolean rememberMeChoice = null;
-        assertThat(rememberMeChoice, is(true));
+        WebElement chkPlannerRemember = driver.findElement(By.id("chkPlannerRemember"));
+        chkPlannerRemember.click();
+        assertThat(chkPlannerRemember.isSelected(), is(true));
     }
 
     @Test
@@ -53,9 +72,11 @@ public class WhenInteractingWithElementsAndForms {
 
         // TODO: Choose the 'Leave After' option and make sure it is the one selected
 
-        String leaveOrArrivePreference = null;
+        WebElement itdTripDateTimeDep = driver.findElement(By.id("itdTripDateTimeDep"));
+       itdTripDateTimeDep.click();
 
-        assertThat(leaveOrArrivePreference, is("arr"));
+        assertThat(itdTripDateTimeDep.isSelected(), is(true));
+        assertThat(itdTripDateTimeDep.getAttribute("value"), is("dep"));
     }
 
 
@@ -63,8 +84,10 @@ public class WhenInteractingWithElementsAndForms {
     public void choose_a_dropdown_value() {
         // TODO: Click on 12pm
 
-        String selectedHour = null;
+        Select itdDate = new Select(driver.findElement(By.id("itdTimeHour")));
+        itdDate.selectByVisibleText("12");
 
+        String selectedHour = itdDate.getFirstSelectedOption().getText();
         assertThat(selectedHour, is("12"));
     }
 
