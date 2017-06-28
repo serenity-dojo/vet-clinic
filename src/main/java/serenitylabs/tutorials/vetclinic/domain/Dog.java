e774422147a3bb93db62c61b995c86a39dcab2a3
John Smart <john.smart@wakaleo.com>
2017-06-28 17:42:59 +0100

    Sample solution

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,15 +1,21 @@
 package serenitylabs.tutorials.vetclinic.domain;
 
+import java.time.LocalDate;
+
 public class Dog {
     private final String name;
     private final String breed;
     private final String colour;
+    private final String favouriteFood;
+    private final LocalDate dateOfBirth;
 
-    public Dog(String name, String breed, String colour) {
+    public Dog(String name, String breed, String colour, String favouriteFood, LocalDate dateOfBirth) {
 
         this.name = name;
         this.breed = breed;
         this.colour = colour;
+        this.favouriteFood = favouriteFood;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public String getName() {
@@ -20,17 +26,31 @@ public class Dog {
         return breed;
     }
 
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public static WithBreed called(String name) {
+        return new DogBuilder(name);
+    }
+
     public String getColour() {
         return colour;
     }
 
-    public static DogBuilder called(String name) {
-        return new DogBuilder(name);
+    interface WithBreed {
+        WithColour ofBreed(String breed);
     }
 
-    public static class DogBuilder {
+    interface WithColour {
+        DogBuilder ofColour(String colour);
+    }
+
+    public static class DogBuilder implements WithBreed, WithColour{
         private final String name;
         private String breed;
+        private String colour;
+        private String favouriteFood;
 
         public DogBuilder(String name) {
             this.name = name;
@@ -41,8 +61,18 @@ public class Dog {
             return this;
         }
 
-        public Dog andOfColour(String colour) {
-            return new Dog(name, breed, colour);
+        public Dog bornOn(LocalDate dateOfBirth) {
+            return new Dog(name, breed, colour, favouriteFood, dateOfBirth);
+        }
+
+        public DogBuilder ofColour(String colour) {
+            this.colour = colour;
+            return this;
+        }
+
+        public DogBuilder withFavouriteFood(String favouriteFood) {
+            this.favouriteFood = favouriteFood;
+            return this;
         }
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -3,14 +3,34 @@ package serenitylabs.tutorials.vetclinic.domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class WhenWeCreateANewDog {
+
+    private final LocalDate THE_FOURTH_OF_JULY = LocalDate.of(2016,07,04);
+
     @Test
-    public void a_new_dog_should_have_a_name_and_a_breed_and_a_colour() throws Exception {
-        Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
+    public void a_new_dog_should_have_a_name_a_breed_a_colour_and_a_date_of_birth() throws Exception {
+        Dog fido = Dog.called("Fido")
+                .ofBreed("Labrador")
+                .ofColour("Black")
+                .bornOn(THE_FOURTH_OF_JULY);
 
         Assert.assertEquals("Fido",fido.getName());
         Assert.assertEquals("Labrador", fido.getBreed());
-        Assert.assertEquals("Black", fido.getColour());
+        Assert.assertEquals(THE_FOURTH_OF_JULY, fido.getDateOfBirth());
     }
 
+    @Test
+    public void a_new_dog_should_optionally_have_a_favourite_food() throws Exception {
+        Dog fido = Dog.called("Fido")
+                      .ofBreed("Labrador")
+                      .ofColour("Black")
+                      .withFavouriteFood("pizza")
+                      .bornOn(THE_FOURTH_OF_JULY);
+
+        Assert.assertEquals("Fido",fido.getName());
+        Assert.assertEquals("Labrador", fido.getBreed());
+        Assert.assertEquals(THE_FOURTH_OF_JULY, fido.getDateOfBirth());
+    }
 }
