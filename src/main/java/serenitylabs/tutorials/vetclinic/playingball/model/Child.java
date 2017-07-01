11b0c7eaf3d946e9588e70c6b40e45ed91843c3f
John Smart <john.smart@wakaleo.com>
2017-07-01 15:07:34 +0100

    Initial tests

--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
@@ -1,15 +1,20 @@
 package serenitylabs.tutorials.vetclinic.playingball.model;
 
-import java.time.LocalDate;
+import static serenitylabs.tutorials.vetclinic.playingball.model.Game.*;
 
 public class Child {
 
     public void goPlay(Game game) {
-        // TODO
-    }
-
-    public Game goPlayBallOn(LocalDate someDay) {
-        // TODO
-        return null;
+        if (game == Football) {
+            System.out.print("Kick the ball");
+        } else if (game == Tennis) {
+            System.out.print("Serve the ball");
+        } else if (game == Cricket) {
+            System.out.print("Hit the wicket");
+        } else if (game == Handball) {
+            System.out.print("Throw the ball");
+        } else if (game == Hockey) {
+            System.out.print("Hit the ball with the stick");
+        }
     }
 }
--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Game.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Game.java
@@ -1,5 +1,5 @@
 package serenitylabs.tutorials.vetclinic.playingball.model;
 
 public enum Game {
-    Football, Tennis, Cricket, Handball;
+    Football, Tennis, Cricket, Handball, Hockey;
 }
