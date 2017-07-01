c7935f8b5f821a9b13a41ab1d9b847b06ed374e1
John Smart <john.smart@wakaleo.com>
2017-07-01 14:01:51 +0100

    Added starting point for the goPlay() method

--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 
 public class Child {
 
+    public void goPlay(Game game) {
+        // TODO
+    }
+
     public Game goPlayBallOn(LocalDate someDay) {
         // TODO
         return null;
