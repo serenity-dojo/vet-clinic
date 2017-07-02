e75c63e3f18cad8df738a88859572506ecdc7d04
John Smart <john.smart@wakaleo.com>
2017-07-02 08:50:51 +0100

    Builder tutorial step 2

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,14 +1,21 @@
 package serenitylabs.tutorials.vetclinic.domain;
 
+import java.time.LocalDate;
+
 public class Dog {
+    private final LocalDate dateOfBirth;
     private String name;
 
-    public Dog(String name) {
-
+    public Dog(String name, LocalDate dateOfBirth) {
         this.name = name;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public String getName() {
         return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -3,12 +3,16 @@ package serenitylabs.tutorials.vetclinic.domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class WhenWeCreateANewDog {
 
+    private final static LocalDate THE_FOURTH_OF_JULY = LocalDate.of(2016,07,04);
     @Test
-    public void a_new_dog_should_have_a_name() {
-        Dog fido = new Dog("Fido");
+    public void a_new_dog_should_have_a_name_and_a_date_of_birth() {
+        Dog fido = new Dog("Fido", THE_FOURTH_OF_JULY);
 
         Assert.assertEquals("Fido",fido.getName());
+        Assert.assertEquals(THE_FOURTH_OF_JULY,fido.getDateOfBirth());
     }
 }
