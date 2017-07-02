05d3cd3bd96336245ae23054e3b79f6db6f5252a
John Smart <john.smart@wakaleo.com>
2017-07-02 09:00:25 +0100

    Builder tutorial step 3

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 
 public class Dog {
     private final LocalDate dateOfBirth;
-    private String name;
+    private final String name;
+    private final String breed;
 
-    public Dog(String name, LocalDate dateOfBirth) {
+    public Dog(String name, String breed, LocalDate dateOfBirth) {
         this.name = name;
+        this.breed = breed;
         this.dateOfBirth = dateOfBirth;
     }
 
@@ -17,5 +19,31 @@ public class Dog {
 
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
+    }
+
+    public static DogBreeder called(String name) {
+        return new DogBreeder(name);
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public static class DogBreeder {
+        private String name;
+        private String breed    ;
+
+        public DogBreeder(String name) {
+            this.name = name;
+        }
+
+        public DogBreeder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog bornOn(LocalDate dateOfBirth) {
+            return new Dog(name, breed, dateOfBirth);
+        }
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -8,11 +8,13 @@ import java.time.LocalDate;
 public class WhenWeCreateANewDog {
 
     private final static LocalDate THE_FOURTH_OF_JULY = LocalDate.of(2016,07,04);
+
     @Test
-    public void a_new_dog_should_have_a_name_and_a_date_of_birth() {
-        Dog fido = new Dog("Fido", THE_FOURTH_OF_JULY);
+    public void a_new_dog_should_have_a_name_a_breed_and_a_date_of_birth() {
+        Dog fido = Dog.called("Fido").ofBreed("Labrador").bornOn(THE_FOURTH_OF_JULY);
 
         Assert.assertEquals("Fido",fido.getName());
+        Assert.assertEquals("Labrador",fido.getBreed());
         Assert.assertEquals(THE_FOURTH_OF_JULY,fido.getDateOfBirth());
     }
 }
