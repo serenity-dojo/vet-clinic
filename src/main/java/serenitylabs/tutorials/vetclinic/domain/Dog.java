22962d333c2878c6134b047817d8f3af32799842
John Smart <john.smart@wakaleo.com>
2017-07-02 09:31:13 +0100

    Builder tutorial step 5

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -7,11 +7,15 @@ public class Dog {
     private final String name;
     private final String breed;
     private final String colour;
+    private final String favouriteFood;
+    private final String favouriteToy;
 
-    public Dog(String name, String breed, String colour, LocalDate dateOfBirth) {
+    public Dog(String name, String breed, String colour, String favouriteFood, String favouriteToy, LocalDate dateOfBirth) {
         this.name = name;
         this.breed = breed;
         this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = favouriteFood;
+        this.favouriteToy = favouriteToy;
         this.colour = colour;
     }
 
@@ -35,20 +39,34 @@ public class Dog {
         return colour;
     }
 
-    public interface WithBreed {
-        DogBreeder ofBreed(String breed);
+    public String getFavouriteFood() {
+        return favouriteFood;
     }
 
-    public static class DogBreeder implements WithBreed {
+    public String getFavouriteToy() {
+        return favouriteToy;
+    }
+
+    public interface WithBreed {
+        WithColour ofBreed(String breed);
+    }
+
+    public interface WithColour {
+        DogBreeder ofColour(String colour);
+    }
+
+    public static class DogBreeder implements WithBreed, WithColour {
         private String name;
         private String breed;
         private String colour;
+        private String favouriteFood;
+        private String favouriteToy;
 
         public DogBreeder(String name) {
             this.name = name;
         }
 
-        public DogBreeder ofBreed(String breed) {
+        public WithColour ofBreed(String breed) {
             this.breed = breed;
             return this;
         }
@@ -59,7 +77,18 @@ public class Dog {
         }
 
         public Dog bornOn(LocalDate dateOfBirth) {
-            return new Dog(name, breed, colour, dateOfBirth);
+            return new Dog(name, breed, colour, favouriteFood, favouriteToy, dateOfBirth);
         }
+
+        public DogBreeder withFavouriteFood(String favouriteFood) {
+            this.favouriteFood = favouriteFood;
+            return this;
+        }
+
+        public DogBreeder withFavouriteToy(String favouriteToy) {
+            this.favouriteToy = favouriteToy;
+            return this;
+        }
+
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -10,24 +10,45 @@ public class WhenWeCreateANewDog {
     private final static LocalDate THE_FOURTH_OF_JULY = LocalDate.of(2016,07,04);
 
     @Test
-    public void a_new_dog_should_have_a_name_a_breed_and_a_date_of_birth() {
-        Dog fido = Dog.called("Fido").ofBreed("Labrador").bornOn(THE_FOURTH_OF_JULY);
+    public void a_new_dog_should_have_a_name_a_breed_a_colour_and_a_date_of_birth() {
+        Dog fido = Dog.called("Fido")
+                .ofBreed("Labrador")
+                .ofColour("black")
+                .bornOn(THE_FOURTH_OF_JULY);
 
         Assert.assertEquals("Fido",fido.getName());
         Assert.assertEquals("Labrador",fido.getBreed());
+        Assert.assertEquals("black",fido.getColour());
         Assert.assertEquals(THE_FOURTH_OF_JULY,fido.getDateOfBirth());
     }
 
     @Test
-    public void a_dog_can_have_an_optional_colour() {
+    public void a_dog_can_have_an_optional_favourite_food() {
         Dog fido = Dog.called("Fido")
                       .ofBreed("Labrador")
                       .ofColour("black")
+                      .withFavouriteFood("pizza")
                       .bornOn(THE_FOURTH_OF_JULY);
 
         Assert.assertEquals("Fido",fido.getName());
         Assert.assertEquals("Labrador",fido.getBreed());
         Assert.assertEquals("black",fido.getColour());
+        Assert.assertEquals("pizza",fido.getFavouriteFood());
+        Assert.assertEquals(THE_FOURTH_OF_JULY,fido.getDateOfBirth());
+    }
+
+    @Test
+    public void a_dog_can_have_an_optional_favourite_toy() {
+        Dog fido = Dog.called("Fido")
+                .ofBreed("Labrador")
+                .ofColour("black")
+                .withFavouriteToy("bone")
+                .bornOn(THE_FOURTH_OF_JULY);
+
+        Assert.assertEquals("Fido",fido.getName());
+        Assert.assertEquals("Labrador",fido.getBreed());
+        Assert.assertEquals("black",fido.getColour());
+        Assert.assertEquals("bone",fido.getFavouriteToy());
         Assert.assertEquals(THE_FOURTH_OF_JULY,fido.getDateOfBirth());
     }
 
