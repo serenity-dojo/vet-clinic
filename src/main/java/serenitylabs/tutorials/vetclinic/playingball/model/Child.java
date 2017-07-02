b3419875021cc18fc10e4474fb4e64aa78efc153
John Smart <john.smart@wakaleo.com>
2017-07-02 23:08:28 +0100

    Simple Strategy+Factory

--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
@@ -1,27 +1,8 @@
 package serenitylabs.tutorials.vetclinic.playingball.model;
 
 public class Child {
-
     public void goPlay(Game game) {
-        Player gameToPlay = gameCalled(game);
+        Player gameToPlay = PlayerForGame.called(game);
         gameToPlay.play();
     }
-
-    private Player gameCalled(Game game) {
-        switch (game) {
-            case Football:
-                return new PlayFootball();
-            case Tennis:
-                return new PlayTennis();
-            case Cricket:
-                return new PlayCricket();
-            case Handball:
-                return new PlayHandball();
-            case Hockey:
-                return new PlayHockey();
-            default:
-                throw new DontKnowThatGameException();
-        }
-    }
-
 }
