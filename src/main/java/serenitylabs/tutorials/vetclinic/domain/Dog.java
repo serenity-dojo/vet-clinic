6ffaa9a620d19574e4de5cf3de922ad1aea7844b
Olena Yurova <aly0naiur0va@gmail.com>
2023-12-19 00:23:30 +0100

    Serenity Dojo 102: Hamcrest exercises
    Serenity Dojo 102: Hamcrest exercises

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,15 +1,25 @@
 package serenitylabs.tutorials.vetclinic.domain;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dog {
     private final String name;
     private final String breed;
-    private final String colour;
+    private final List<String> colour;
 
-    public Dog(String name, String breed, String colour) {
+    public Dog(String name, String breed, List<String> colour) {
 
         this.name = name;
         this.breed = breed;
         this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return name + " the " + (colour + " " + breed).toLowerCase();
     }
 
     public String getName() {
@@ -20,8 +30,8 @@ public class Dog {
         return breed;
     }
 
-    public String getColour() {
-        return colour;
+    public List<String> getColour() {
+        return new ArrayList(colour);
     }
 
     public static DogBuilder called(String name) {
@@ -41,8 +51,8 @@ public class Dog {
             return this;
         }
 
-        public Dog andOfColour(String colour) {
-            return new Dog(name, breed, colour);
+        public Dog andOfColour(String... colour) {
+            return new Dog(name, breed, ImmutableList.copyOf(colour));
         }
     }
 }
