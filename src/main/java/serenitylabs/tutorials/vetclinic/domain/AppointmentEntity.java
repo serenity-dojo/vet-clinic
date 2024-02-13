5a1f6ec5b46fd863dfa4fde4c75c9033cd9b770d
abhinay5993 <abhinaylunawat5993@gmail.com>
2024-02-14 02:15:01 +0530

    test script implementation for 'Vet Clinic Tutorial 2 - Hamcrest Matchers' Exercises Step 5 , Step 6 added

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/AppointmentEntity.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/AppointmentEntity.java
@@ -1,5 +1,55 @@
 package serenitylabs.tutorials.vetclinic.domain;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class AppointmentEntity {
 
+	private final String petName;
+	private final String owner;
+	private final LocalDateTime appointmentTime;
+	private final Optional<String> reason;
+	
+	public AppointmentEntity(String petName, String owner, LocalDateTime appointmentTime,String reason) {
+		this.petName = petName;
+		this.owner = owner;
+		this.appointmentTime = appointmentTime;
+		this.reason = Optional.ofNullable(reason);
+	}
+
+	public AppointmentEntity(String petName, String owner, LocalDateTime appointmentTime) {
+		this(petName, owner, appointmentTime, null);
+	}
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public LocalDateTime getAppointmentTime() {
+		return appointmentTime;
+	}
+
+	public Optional<String> getReason() {
+		return reason;
+	}
+	
+	public boolean isBefore(LocalDateTime isBeforeLocatDt)
+	{
+	return true;
+	}
+	
+	public boolean isAfter(LocalDateTime isAfterLocatDt)
+	{
+	return true;
+	}
+	
+	
+	public static AppointmentBookerBuilderEntity forPetCalled(String petName) {
+		return new AppointmentBookerBuilderEntity(petName);
+
+	}
+	  
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeBookAnAppointmentTests.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeBookAnAppointmentTests.java
@@ -1,5 +1,50 @@
 package serenitylabs.tutorials.vetclinic.domain;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import java.time.LocalDateTime;
+import org.testng.annotations.Test;
 
 public class WhenWeBookAnAppointmentTests {
+
+	private static final LocalDateTime TODAY_AT_2_PM = LocalDateTime.now().withHour(2).withMinute(0);
+	
+	/*
+	 * This TestNg test is representing test validation for 'Vet Clinic Tutorial 2 -
+	 * Hamcrest Matchers' modules Exercise - 'Step 5 - Updating the
+	 * WhenWeBookAnAppointmentTests test' implementation
+	 * 
+	 */
+	@Test
+	public void an_appointment_has_a_patient_name_an_owner_and_a_date() {
+		AppointmentEntity appointObj = AppointmentEntity.forPetCalled("Fido")
+				                            .ownedBy("Fred")
+				                            .at(TODAY_AT_2_PM);
+
+		assertThat(appointObj.getPetName(),is(equalTo("Fido")));
+		assertThat(appointObj.getOwner(),is(equalTo("Fred")));
+		assertThat(appointObj.getAppointmentTime(),is(equalTo(TODAY_AT_2_PM)));
+		System.out.println("Execution of - 'Step 5 - Updating the WhenWeBookAnAppointment tests' completed.");
+	}
+
+	
+	/*
+	 * This TestNg test is representing test validation for 'Vet Clinic Tutorial 2 -
+	 * Hamcrest Matchers' modules Exercise - 'Step 6 - comparing appointments'
+	 * implementation
+	 * 
+	 */
+	@Test
+	public void an_appointment_can_have_an_optional_reason() {
+		AppointmentEntity appointObj = AppointmentEntity.forPetCalled("Fido")
+				                            .ownedBy("Fred")
+				                            .because("He is sick")
+				                            .at(TODAY_AT_2_PM);
+
+		assertThat(appointObj.getReason().isPresent(),is(true));
+		assertThat(appointObj.getReason().get(),is(equalTo("He is sick")));
+		System.out.println("Execution of - 'Step 6 - comparing appointments' tests completed.");
+	}
+
 
 }
